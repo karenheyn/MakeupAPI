c9aff645a4316f417228a428eeb5aec554b10129
karenheyn <karenheyn@gmail.com>
2019-11-13 20:42:02 -0500

    working on routing

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+
+app.use(parser.json());
+app.use(require("./lib/routes/index"));
+
+app.listen(4000, () => console.log("listening for mixers on port 3000"));
