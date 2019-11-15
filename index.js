38bc5b9722513a0a2260407dd1da0aa15b1684c0
karenheyn <karenheyn@gmail.com>
2019-11-15 10:32:30 -0500

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const parser = require("body-parser");
-
+const cors = require("cors");
 app.use(parser.json());
-
+app.use(cors());
 app.use(require("./lib/routes/index"));
 
 app.set("port", process.env.PORT || 4000);
